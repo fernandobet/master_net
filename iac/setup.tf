391f4ebee9fe938fc9b2f5006ee3d18c7160ff20
fernando betancourt compean <fernando.mbc@hotmail.com>
2025-11-20 15:00:21 -0600

    add new workflow for iac

--- iac/setup.tf
+++ iac/setup.tf
@@ -5,6 +5,14 @@ terraform{
         version = "4.53.0"
       }
     }
+//Con esto le diremos a terraform que el archivo .tfstate ahora lo almacenara
+//en la nube y no localmente. Se volvio a ejecutar el comando terraform init
+    backend "azurerm"{
+      resource_group_name = "nu-iac-dev-eus-rg"
+      storage_account_name = "nuiacdeveusact"
+      container_name = "terraform"
+      key = "terraform.tfstate"
+    }
 }
 
 provider "azurerm" {
